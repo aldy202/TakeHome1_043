ff3c729740ce61f0296e42c1a5e6e558ba71492c
aldy202 <aldyfauzi202@.gmail.com>
2022-10-31 20:05:21 +0700

    make java controller

--- src/main/java/pws/praktikum2/dataForm.java
+++ src/main/java/pws/praktikum2/dataForm.java
@@ -5,10 +5,44 @@
  */
 package pws.praktikum2;
 
+import java.io.IOException;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
- * @author hp
+ * @author aldy ahmad fauzi
  */
+@Controller
 public class dataForm {
+    
+    @RequestMapping("/data")
+    public String form(@RequestParam(value = "variableA") String name,
+            @RequestParam(value = "variableB") String location,
+            @RequestParam(value = "variableC") MultipartFile photo,
+            Model antar)
+            
+            
+    throws IOException{
+        
+        byte[] imge = photo.getBytes();
+        String base64 = Base64.encodeBase64String(imge);
+        String gambar = "data:image/png;base64".concat(base64);
+        
+        antar.addAttribute("antar1", name);
+        antar.addAttribute("antar2", location);
+        antar.addAttribute("antar3", gambar);
+        
+        return "viewhalaman";
+        
+        
+        
+        
+    }
+    
     
 }
